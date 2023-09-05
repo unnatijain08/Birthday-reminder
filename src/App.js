7a8a5aa6b10381e23c603b810ca712c9524e1ebf
Unnati Jain <unnati.del@gmail.com>
2023-09-05 15:41:00 +0530

    Birthday-reminder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import data from "./data";
+import "./App.css";
+import "./style.css";
+{
+  /* <style> */
+}
 
 function App() {
+  const [template, setTemplate] = useState([]);
+
+  useEffect(() => {
+    setTemplate(data);
+  }, []);
+
+  const cake = (NAME) => {
+    const newData = template.filter((item) => item.NAME !== NAME);
+    setTemplate(newData);
+    // console.log(newData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button class="head" id="s" >
+        <h1 class="head"> BIRTHDAY REMINDER </h1>
+      </button>
+      <br />
+      <br />
+      {template.map((human) => {
+        return (
+          <>
+            <button class="main" >
+              <div class="main" id="m" widht="50px">
+              <h4 id="sno">{human.SNO}</h4>
+              <img src={human.Image} width="160pxl" />
+              <h1>{human.NAME}</h1>
+              <h2>{human.BIRTHDAY}</h2>
+              <button id="but"
+                onClick={() => {
+                  cake(human.NAME);
+                }}
+                >
+                {" "}
+                REMOVE
+              </button>
+                </div>
+            </button>
+            
+          </>
+        );
+      })}
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default[
+    {
+        SNO : 1,
+        NAME : "UNNATI JAIN",
+        BIRTHDAY : "8-NOVEMBER",
+        Image : "https://cakewithname.net/pic-preview/Unnati/123/0/beautiful-best-birthday-cake-for-Unnati.jpg",
+    },
+    {
+        SNO : 2,
+        NAME : "AVNI JAIN",
+        BIRTHDAY : "26-MARCH",
+        Image : "https://mynamepix.com/names/images/happy-birthday-avni_35485b9666.jpg",
+    },
+    {
+        SNO : 3,
+        NAME : "ANSH GARG",
+        BIRTHDAY : "3-APRIL",
+        Image : "http://mynamepixs.com/result_display.php?id=2220&name=Ansh&name2=",
+    },
+    {
+        SNO : 4,
+        NAME : "ANJALI GARG",
+        BIRTHDAY : "27-JANUARY",
+        Image : "https://i.pinimg.com/736x/29/e8/4e/29e84e6f38ef56eab3c47d0b7c082864.jpg",
+    },  
+];
